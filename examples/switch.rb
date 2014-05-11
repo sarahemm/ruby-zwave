bc0227ca4a085d74b7de554e232c23ef754fbfa3
sen <github@sen.cx>
2014-05-11 11:20:47 -0400

    Adding some status print-outs during the switch example.

--- examples/switch.rb
+++ examples/switch.rb
@@ -5,7 +5,15 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'zwave'
 
 zwave = ZWave::SerialAPI.new ARGV[0]
+unit_id = ARGV[1].to_i
 zwave.debug = true
-zwave.switch(ARGV[1].to_i).switch_off
+
+zwave.switch(unit_id).switch_off
+sleep 0.5
+p zwave.get(unit_id, ZWave::SerialAPI::Constants::CommandClass::SWITCH_BINARY)
+
 sleep 2
-zwave.switch(ARGV[1].to_i).switch_on
+
+zwave.switch(unit_id).switch_on
+sleep 0.5
+p zwave.get(unit_id, ZWave::SerialAPI::Constants::CommandClass::SWITCH_BINARY)
