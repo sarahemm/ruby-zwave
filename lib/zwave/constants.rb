dfdea19ec24ae164f9de567360b4c68e982fc6ac
sen <github@sen.cx>
2014-05-11 11:21:02 -0400

    Adding lots more constants reversed out and found in public docs.

--- lib/zwave/constants.rb
+++ lib/zwave/constants.rb
@@ -25,20 +25,17 @@ module ZWave
       # name from Z-Way API manual Dec 2012
       class FunctionClass
         # value from reversing
-        SEND_DATA = 0x13
+        SEND_DATA               = 0x13
         # value from reversing
-        GET_NODE_PROTOCOL_INFO = 0x41
+        GET_NODE_PROTOCOL_INFO  = 0x41
+        # value from reversing
+        DEVICE_UPDATE           = 0x49
       end
       
       # name from Electronic Solutions DBMZ v2+1 manual
       class CommandClass
         # name from Leviton RZC0P programming guide v1.2, value from reversing
         BASIC                     = 0x20
-        # name from Leviton RZC0P programming guide v1.2, value not yet known
-        #MULTILEVEL_SWITCH = 
-        # names from Electronic Solutions DBMZ v2+1 manual, values not yet known
-        #SWITCH_ALL = 
-        #MFG_SPECIFIC_GET = 
         # name and value from http://wiki.micasaverde.com/index.php/ZWave_Command_Classes
         ASSOCIATION               = 0x85
         # names and values from BeNext Quick start: Tag Reader EU (www.benext.eu/static/manual/tagreader.pdf)
@@ -50,6 +47,28 @@ module ZWave
         BATTERY                   = 0x80
         WAKE_UP                   = 0x84
         VERSION                   = 0x86
+        # names and values from BeNext Quick start: Internet Gateway EU (www.benext.eu/static/manual/internetgateway.pdf)
+        SWITCH_ALL                = 0x27
+        METER                     = 0x32
+        MULTI_CMD                 = 0x8F
+        MULTI_CHANNEL_ASSOCIATION = 0x8E
+        MULTI_CHANNEL             = 0x60
+        POWER_LEVEL               = 0x73
+        PROTECTION                = 0x75
+        SCREEN_ATTRIBUTES         = 0x93
+        SENSOR_BINARY             = 0x30
+        SENSOR_MULTILEVEL         = 0x31
+        SWITCH_MULTILEVEL         = 0x26
+        # names and values from Zipato Micro Motor Controller Quick Install Guide v1.2 (http://www.homecontrols.com/homecontrols/products/pdfs/ZP-Zipato/ZPWTMMMCUS_Installation.pdf)
+        NAMING_AND_LOCATION       = 0x77
+        INDICATOR                 = 0x87
+        SCENE_ACTIVATION          = 0x2B
+        SCENE_ACTUATOR_CONFIG     = 0x2C
+        # things we've seen in reversing but don't know what they are yet
+        COMMAND_CLASS_UNKNOWN_1   = 0x91 # seen on the Leviton VRCS4-MRZ
+        COMMAND_CLASS_UNKNOWN_2   = 0x2D # seen on the Leviton VRCS4-MRZ
+        COMMAND_CLASS_UNKNOWN_3   = 0x7C # seen on the Leviton VRCS4-MRZ
+        COMMAND_CLASS_UNKNOWN_4   = 0x82 # seen on the Leviton VRCS4-MRZ
       end
       
       class Command
@@ -94,6 +113,11 @@ module ZWave
         THERMOSTAT        = 0x08
         SWITCH            = 0x10
         DIMMER            = 0x11
+      end
+      
+      # names made up, values from reversing
+      class DeviceUpdate
+        STATUS_UPDATE     = 0x84
       end
     end
   end
